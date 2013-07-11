b0e1848a69f3f8ee891f02559d5b7f55e1c87c34
Tuan <tuandnvn1318@gmail.com>
2013-07-11 20:21:52 +0000

    find main.css

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var buffer = new Buffer( fs.readFileSync('index.html') );
+var buffer = new Buffer( fs.readFileSync('main.css') );
 var str = buffer.toString('utf8');
 
 var app = express.createServer(express.logger());
